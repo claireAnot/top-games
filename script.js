743dbd996866efadfe64c67c83f6ca9b20b49274
Claire ANOT DELCOURT <claire.anotd@gmail.com>
2024-11-29 15:20:54 +0100

    Préparation du formulaire pour modification des jeux, enregistrement sur GitHub

--- script.js
+++ script.js
@@ -56,8 +56,24 @@ function writeDom() {
                 <p class="card-text">${game.year}</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
+                        <button 
+                            type="button" 
+                            class="btn btn-sm btn-outline-secondary view" 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#exampleModal"
+                            data-edit-id="${game.id}"
+                            >
+                                View
+                        </button>
+                        <button 
+                            type="button" 
+                            class="btn btn-sm btn-outline-secondary edit" 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#exampleModal"
+                            data-edit-id="${game.id}"
+                            >
+                                Edit
+                        </button>
                     </div>
                 </div>
             </div>
@@ -68,3 +84,33 @@ function writeDom() {
 }
 
 writeDom()
+const editButtons = document.querySelectorAll(".edit")
+editButtons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		editModal(e.target.getAttribute("data-edit-id"))
+	})
+})
+
+function editModal(gameId) {
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
+    modifyModal(gamesList[result].title, modalBody)
+}
+
+const viewButtons = document.querySelectorAll(".view")
+viewButtons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		viewModal(e.target.getAttribute("data-edit-id"))
+	})
+})
+
+function viewModal(gameId) {
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
+    modifyModal(gamesList[result].id, modalBody)
+}
+
+function modifyModal(modalTitle, modalBody) {
+	document.querySelector(".modal-title").textContent = modalTitle
+    document.querySelector(".modal-body").innerHTML = modalBody
+}
